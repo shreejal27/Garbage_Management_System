a8af4dcee927a31d999ce11bf54ac2905b5ef04b
Shreejal Maharjan <shreejal27@gmail.com>
2023-01-06 21:54:31 +0545

    added categories

--- scrap_display.php
+++ scrap_display.php
@@ -1,7 +1,7 @@
 <?php require_once "1)config.php"; ?>
 <?php
 
-$query= "Select * from scrap";
+$query= "Select * from scrap LIMIT 5";
 
 $result= mysqli_query($connection, $query);
 ?>
@@ -9,27 +9,26 @@ $result= mysqli_query($connection, $query);
 
  <link rel="stylesheet" href="scrap_display.css" />
 
-
-
 <form action="scrap_searchresult.php" method="POST">
   <input type="text" name="result" placeholder="Search anything"  >
 
   <button type="submit"> <img src="icons/search.png" > </button>
 </form>
+<div class= "row">
 
-    <h2> Most Sold</h2>
-
-        <?php
+  <h2> Most Sold</h2>
+  
+  <?php
     while($row= mysqli_fetch_assoc($result)){
-        ?>
+      ?>
            <a class="link" href="scrap_details.php?id=<?php echo $row['id'];?>">
-
+            
      
        
         <div class="box">
 
           <div class="image">
-
+            
             <span><img src ="scrap/<?php echo $row["simage"]?>" height = '240px' width= '220px'></span> <br><br>
           </div>
     
@@ -37,18 +36,188 @@ $result= mysqli_query($connection, $query);
 
            <span class="title"> 
              <?php echo $row["sname"]?> <br> 
-
+             
            </span>
            <span class="price">
-
+             
              <?php echo $row["sprice"]?><br>
-           </span>
+            </span>
           </div>
-         <br>
-           <br>
-
+          <br>
+          <br>
+          
         </div>
-        </a>
-        <?php 
+      </a>
+      <?php 
         }
         ?>
+
+</div>
+
+
+<h1> Categories</h1>
+
+<div class= "row">
+
+  <h2> Plastic</h2>
+  
+  <?php
+$query1= "Select * from scrap where category = 'plastic' ";
+
+$result1= mysqli_query($connection, $query1);
+while($row1= mysqli_fetch_assoc($result1)){
+?>
+   <a class="link" href="scrap_details.php?id=<?php echo $row1['id'];?>">
+
+   
+   
+   <div class="box">
+     
+     <div class="image">
+
+    <span><img src ="scrap/<?php echo $row1["simage"]?>" height = '240px' width= '220px'></span> <br><br>
+  </div>
+
+ <div class="details">
+
+   <span class="title"> 
+     <?php echo $row1["sname"]?> <br> 
+
+   </span>
+   <span class="price">
+
+     <?php echo $row1["sprice"]?><br>
+   </span>
+  </div>
+ <br>
+ <br>
+ 
+</div>
+</a>
+<?php 
+}
+?>
+</div>
+
+<div class= "row">
+<h2> Paper</h2>
+
+<?php
+$query1= "Select * from scrap where category = 'paper' ";
+
+$result1= mysqli_query($connection, $query1);
+while($row1= mysqli_fetch_assoc($result1)){
+?>
+   <a class="link" href="scrap_details.php?id=<?php echo $row1['id'];?>">
+
+
+  
+<div class="box">
+  
+  <div class="image">
+
+    <span><img src ="scrap/<?php echo $row1["simage"]?>" height = '240px' width= '220px'></span> <br><br>
+  </div>
+
+ <div class="details">
+
+   <span class="title"> 
+     <?php echo $row1["sname"]?> <br> 
+
+   </span>
+   <span class="price">
+
+     <?php echo $row1["sprice"]?><br>
+   </span>
+  </div>
+ <br>
+   <br>
+
+</div>
+</a>
+<?php 
+}
+?>
+</div>
+
+<div class= "row">
+<h2> Ewaste</h2>
+
+<?php
+$query1= "Select * from scrap where category = 'ewaste' ";
+
+$result1= mysqli_query($connection, $query1);
+while($row1= mysqli_fetch_assoc($result1)){
+?>
+   <a class="link" href="scrap_details.php?id=<?php echo $row1['id'];?>">
+
+
+  
+<div class="box">
+  
+  <div class="image">
+
+    <span><img src ="scrap/<?php echo $row1["simage"]?>" height = '240px' width= '220px'></span> <br><br>
+  </div>
+
+ <div class="details">
+
+   <span class="title"> 
+     <?php echo $row1["sname"]?> <br> 
+
+   </span>
+   <span class="price">
+
+     <?php echo $row1["sprice"]?><br>
+   </span>
+  </div>
+ <br>
+   <br>
+
+</div>
+</a>
+<?php 
+}
+?>
+</div>
+
+<div class= "row">
+<h2> Glass</h2>
+
+<?php
+$query1= "Select * from scrap where category = 'glass' ";
+
+$result1= mysqli_query($connection, $query1);
+while($row1= mysqli_fetch_assoc($result1)){
+?>
+   <a class="link" href="scrap_details.php?id=<?php echo $row1['id'];?>">
+
+
+  
+<div class="box">
+  
+  <div class="image">
+
+    <span><img src ="scrap/<?php echo $row1["simage"]?>" height = '240px' width= '220px'></span> <br><br>
+  </div>
+
+ <div class="details">
+
+   <span class="title"> 
+     <?php echo $row1["sname"]?> <br> 
+
+   </span>
+   <span class="price">
+
+     <?php echo $row1["sprice"]?><br>
+   </span>
+  </div>
+ <br>
+   <br>
+
+</div>
+</a>
+<?php 
+}
+?>
+</div>
--- scrap_sellnow.php
+++ scrap_sellnow.php
@@ -12,12 +12,16 @@ $row = mysqli_fetch_array($data);
 $cookie= $_COOKIE['quantity'];
 ?>
 <style>
+    body{
+        margin-left: 10%;
+        margin-right: 10%;
+    }
 #addressbar{
     width: 100%;
     height: 20px;
     padding: 30px 0;
     outline: 1px solid black;
-    margin: 20px;
+    margin: 20px 0px; 
    
     border-radius: 10px;
 }
@@ -28,42 +32,91 @@ a{
 
 #productbox{
     width: 100%;
-    height: 250px;
-    background-color: blue;
+    height: 450px;
+    outline: 1px solid black;
     border-radius: 10px;
+    padding: 10px;
+    float: left;
 }
+
+.details{
+    float:left; 
+    font-size: 32px;
+    margin: 10px;
+    margin-top: 30px;
+    margin-left: 20%;
+}
+.pickup{
+    height: 100px;
+    width: 200px;
+    outline: 1px solid black;
+    margin-top: 150px;
+    font-size: 20px;
+    padding: 5px;
+    text-align: center;
+    border-radius: 5px;
+    
+}
+.sell{
+    margin-top: 30px;
+    float: right; 
+    
+    font-size: 20px;
+}
+button{
+    height: 40px;
+    width: 200px;
+    float:right;
+    margin-top: 10px;
+    border-radius: 5px;
+    
+}
+.button{
+    font-size: 16px;
+}
+
     </style>
 
 <center>
     
     <div id="addressbar">
-        
         <a href="scrap_address.php"> Add Your Address </a>
     </div>
 </center>
     
     <div id = "productbox">
-        <p> Sell Now </p>
+        <p style="font-size:24px;"> Sell Now </p>
     
-    <span class="image">
-        
-        <img src ="scrap/<?php echo $row["simage"]?>" height = '50px' width= '50px'>
-    </span>
-    <span style="font-size: 30px;"> 
+    <div class="image" style="float:left; ">
+        <img src ="scrap/<?php echo $row["simage"]?>" height = '100px' width= '100px'>
+    </div>
+    
+    <div class = "details">  
         <?php echo $row["sname"]?> 
-    </span>
-    <span>
+    </div>
+    <div class = "details">
         Qty: <?php echo $cookie?>
-    </span>
-    <span>
+    </div>
+    <div class = "details">
         Rs: <?php echo $row['sprice']?>
-    </span>
+    </d>
+    </div>
+    <!-- <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br> -->
+    <div class="pickup">
+        <p>Standard PickUp</p>
+        <p>Free</p>   
+     </div>
 
-</div>
-<div style="float: right; background-color: yellow";>
-    SubTotal: Rs. <?php echo ($cookie* $row['sprice'])?>
-</div>
-<br>
-<a href="scrap_confirm" style="float: right;"><button> Confirm Sell</button></a>
-</div>
+        <div class="sell">
+            SubTotal: Rs. <?php echo ($cookie* $row['sprice'])?>
+            <br>
+            <button> <a class= "button" href="scrap_confirm">Confirm Sell</a></button>
+        </div>
 
+
+        </div>
